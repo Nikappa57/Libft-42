4bb3237d39c42d268cb06e562febcd0213500e4e
Nikappa57 <lorenz.gaudino@gmail.com>
2023-01-25 23:40:51 +0100

    Libc functions

---
+++ ft_strlcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcopy.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lorenzogaudino <lorenzogaudino@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/25 10:22:35 by lgaudino          #+#    #+#             */
+/*   Updated: 2023/01/25 18:41:34 by lorenzogaud      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < dstsize - 1 && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	if (dstsize)
+		dst[dstsize] = '\0';
+	return (ft_strlen(src));
+}
