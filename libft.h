ad5197fa6db887c080fcda5c136acd49eb8400d9
Lorenzo Gaudino <49768358+Nikappa57@users.noreply.github.com>
2023-01-25 13:10:36 +0100

    Create libft.h

---
+++ libft.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgaudino <lgaudino@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/23 11:26:55 by lgaudino          #+#    #+#             */
+/*   Updated: 2023/01/25 12:43:00 by lgaudino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_sascii(int c);
+int		ft_isprint(int c);
+
+size_t	ft_strlen(const char *s);
+size_t	strlcpy(char *restrict dst, const char *restrict src, size_t dstsize);
+size_t	strlcat(char *restrict dst, const char *restrict src, size_t dstsize);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+// ...
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+// ...
+char	*ft_strtrim(char const *s1, char const *set);
+
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+
+#endif
