f5bf7287d51b6443a3a4f252ce65982f9fe5103c
Nikappa57 <lorenz.gaudino@gmail.com>
2023-01-25 23:42:12 +0100

    Libc functions

---
+++ ft_memchr.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lorenzogaudino <lorenzogaudino@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/25 22:43:39 by lorenzogaud       #+#    #+#             */
+/*   Updated: 2023/01/25 22:55:36 by lorenzogaud      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	while (n--)
+	{
+		if (*((unsigned char *)s) == (unsigned char)c)
+			return ((void *)s);
+		s++;
+	}
+	return (0);
+}
